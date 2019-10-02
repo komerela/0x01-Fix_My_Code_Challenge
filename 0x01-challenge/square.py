c2b551d2a4e71bbc708fc87a9f4160893bde6f7d
komerela <kmwongera@icloud.com>
2019-10-02 23:42:00 +0000

    Print square if width & height are equal by changing integers

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -12,13 +12,10 @@ class square:
 
     def area_of_my_square(self):
         """ Area of the square """
-        if self.width == self.height:
-            return self.width * self.height
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
-        """ Perimeter of square"""
-        if self.width == self.height:
-            return (self.width * 2) + (self.height * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
@@ -26,7 +23,7 @@ class square:
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square(width=9, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
